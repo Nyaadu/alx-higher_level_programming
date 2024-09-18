91ad39c07ef01a908f98bba5de8cbe26acaf2593
Nyaadu <nyaadu@gmail.com>
2024-09-18 10:05:43 -0700

    corrections

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (ist(map(lambda value: list(map(lambda v2: v2**2, value)), matrix))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    average = 0
+    div = 0
+    for tup in my_list:
+        average += tup[0] * tup[1]
+        div += tup[1]
+    return float(average / div)
