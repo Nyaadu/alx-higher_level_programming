6765a3d10ef23cc90b128ef40098579add05991e
Nyaadu <nyaadu@gmail.com>
2024-10-07 13:09:29 -0700

    2

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,7 +3,9 @@
 nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
-from sys import argv
+
+import sys
+
 if __name__ == "__main__":
     a = []
     if len(argv) != 2:
